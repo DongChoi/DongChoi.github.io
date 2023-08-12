9b68cc169e9acc8945c919318d7c204d8ca814c4
DongChoi <dongandrewchoi@gmail.com>
2023-08-12 11:40:53 -0700

    initial commit to new website

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hi! lets get started
+      <ol>
+        <li>make navigation</li>
+        <li>git commit {"->"} push</li>
+        <li>
+          Migrate all contents from legacy personal website *make component for
+          it*
+        </li>
+        <li>git commit {"->"} push</li>
+        <li>convert contents from prev step to different links{"(pages)"}</li>
+        <li>git commit {"->"} push</li>
+        <li>style the page</li>
+        <li>git commit {"->"} push</li>
+        <li>make animations</li>
+        <li>git commit {"->"} push</li>
+      </ol>
+    </div>
+  );
+}
